7a36c150ac3d67aeb3945f967c93f864ac66eb90
ykjellin <127968935+ykjellin@users.noreply.github.com>
2025-02-11 11:43:29 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "bootstrap/scss/bootstrap";`,
+      },
+    },
+  },
   server: {
     open: true,
     hmr: true,
