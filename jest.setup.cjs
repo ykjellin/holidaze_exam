2ab1d5834253caabb720d012929a9c63414aee5e
ykjellin <127968935+ykjellin@users.noreply.github.com>
2025-02-12 14:30:07 +0100

    Jest removal

--- jest.setup.cjs
+++
@@ -1,6 +0,0 @@
-require("@testing-library/jest-dom");
-
-const { TextEncoder, TextDecoder } = require("util");
-
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
--- src/tests/Footer.test.tsx
+++
@@ -1,9 +0,0 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import { Footer } from "../components/layout/Footer.tsx";
-
-test("renders footer text", () => {
-  render(<Footer />);
-  const footerText = screen.getByText(/© 2025 Holidaze/i);
-  expect(footerText).toBeInTheDocument();
-});
